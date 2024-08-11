b23f2f959bdc74ea5eaf80915ce0aba07ee45292
gsouzza33 <gsouzanasciment@gmail.com>
2024-08-11 14:09:57 -0300

    Juan

---
+++ script.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+// Animação de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
